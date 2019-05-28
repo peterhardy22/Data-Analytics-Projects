2edd31a47f4b8f70304c52a2ab42184815ef414a
Peter Hardy <peterphardy@gmail.com>
2019-05-27 17:07:10 -0700

    progress

--- 13 - Reporting Aliens With Javascript/static/js/app.js
+++ 13 - Reporting Aliens With Javascript/static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Use D3 to select the table
+var table = d3.select("table");
+
+// Use d3 to create a bootstrap striped table
+table.attr("class", "ufo-table");
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the alien sighting data from data.js
+console.log(data);
+
+// Use D# to add data to 
+data.forEach((alienSighting) => {
+  var row = tbody.append("tr");
+  Object.entries(alienSighting).forEach(([key, value]) => {
+    var cell = tbody.append("td");
+    cell.text(value);
+  });
+});

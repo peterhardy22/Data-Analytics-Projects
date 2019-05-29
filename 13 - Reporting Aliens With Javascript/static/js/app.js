e6d8ece538f491cb10ab44c73673086859acf758
Peter Hardy <peterphardy@gmail.com>
2019-05-28 20:11:36 -0700

    progress

--- 13 - Reporting Aliens With Javascript/static/js/app.js
+++ 13 - Reporting Aliens With Javascript/static/js/app.js
@@ -1,6 +1,9 @@
 // from data.js
 var tableData = data;
 
+var res = tableData.toUpperCase();
+
+// APPENDS TABLE TO WEB PAGE //
 // Use D3 to select the table
 var table = d3.select("table");
 
@@ -20,4 +23,28 @@ data.forEach((alienSighting) => {
     var cell = tbody.append("td");
     cell.text(value);
   });
+});
+
+// DATE FORM SET UP
+// Select the submit button
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+  console.log(filteredData);
+
 });

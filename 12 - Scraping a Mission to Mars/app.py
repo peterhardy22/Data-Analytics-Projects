24d37fbdc4421d61bd0a97ed0fe886c474f6d965
Peter Hardy <peterphardy@gmail.com>
2019-06-01 17:02:59 -0700

    I can almost see the light

--- 12 - Scraping a Mission to Mars/app.py
+++ 12 - Scraping a Mission to Mars/app.py
@@ -7,40 +7,24 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_data_app"
-mongo = PyMongo(app)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-@app.route("/")
+@app.route('/')
 def index():
-    mars_data = mongo.db.mars.find_one()
-    return render_template("index.html", mars_data = mars_data)
+    mars_data = mongo.db.mars_data_db.find_one()
+    return render_template("index.html", marsdata = mars_data)
 
 @app.route("/scrape")
 def scrape():
+    mars_collection = mongo.db.mars_data_db
     mars_data = scrape_mars.scrape()
-    mongo.db.mars.update({}, mars_data, upsert=True)
-    # Redirect back to home page
+    mars_collection.update(
+        {},
+        mars_data,
+        upsert=True
+    )
     return redirect("/", code=302)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

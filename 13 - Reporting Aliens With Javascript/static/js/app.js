85458412efe2dc97407334b83a2764757defc20f
Peter Hardy <peterphardy@gmail.com>
2019-06-03 13:46:20 -0700

    finished project

--- 13 - Reporting Aliens With Javascript/static/js/app.js
+++ 13 - Reporting Aliens With Javascript/static/js/app.js
@@ -13,38 +13,40 @@ console.log(data);
 data.forEach(function(alienSighting) {
   console.log(alienSighting);
   var row = tbody.append("tr");
-   Object.entries(alienSighting).forEach(function([key, value]) {
-     console.log(key, value);
-     var cell = tbody.append("td");
-     cell.text(value);
-   });
- });
+  Object.entries(alienSighting).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = tbody.append("td");
+    cell.text(value);
+  });
+});
 
 
 // CREATE FILTER BY DATE //
-
+// Create variable for the Filter Table button
 var submit = d3.select("#submit");
 
- // function to take input and recreate table
- submit.on("click", function() {
-   // stops page from refreshing
-   d3.event.preventDefault();
- 
-   d3.select(".summary").html("");
- 
-   // user input as variable
-   var inputElement = d3.select("#datetime");
-   var inputValue = inputElement.property("value");
- 
-   // Filter Data
-   var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
- 
-   // loop through
-   filteredData.forEach((dateData) => {
-     var row = tbody.append("tr");
-     Object.entries(dateData).forEach(([key, value]) => {
-       var cell = tbody.append("td");
-       cell.text(value);
-     });
-   });
- });
+// Function to take input and recreate table
+submit.on("click", function() {
+
+  // Stops page from refreshing
+  d3.event.preventDefault();
+
+  // Selects table in tbody tag
+  d3.select(".summary").html("");
+
+  // Create variables for input element and value
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+
+  // Filter tableData based on datetime element
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+  // Loop through filteredData
+  filteredData.forEach((dateData) => {
+    var row = tbody.append("tr");
+    Object.entries(dateData).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+});

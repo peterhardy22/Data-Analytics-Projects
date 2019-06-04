c8c7291a8b1aea4f967ec7fabeac2b8efbed76e6
Peter Hardy <peterphardy@gmail.com>
2019-06-03 18:20:21 -0700

    added some of task 2

--- 13 - Reporting Aliens With Javascript/static/js/app.js
+++ 13 - Reporting Aliens With Javascript/static/js/app.js
@@ -35,7 +35,7 @@ submit.on("click", function() {
   d3.select(".summary").html("");
 
   // Create variables for input element and value
-  var dateInput = d3.select("#datetime");
+  var dateInput = d3.select("#input");
   var dateValue = dateInput.property("value");
 
   // Filter tableData based on datetime element
@@ -51,111 +51,111 @@ submit.on("click", function() {
   });
 });
 
-// CREATE FILTER BY CITY //
-// Function to take input and recreate table
-submit.on("click", function() {
+// // CREATE FILTER BY CITY //
+// // Function to take input and recreate table
+// submit.on("click", function() {
 
-  // Stops page from refreshing
-  d3.event.preventDefault();
+//   // Stops page from refreshing
+//   d3.event.preventDefault();
 
-  // Selects table in tbody tag
-  d3.select(".summary").html("");
+//   // Selects table in tbody tag
+//   d3.select(".summary").html("");
 
-  // Create variables for input element and value
-  var cityInput = d3.select("#city");
-  var cityValue = cityInput.property("value");
+//   // Create variables for input element and value
+//   var cityInput = d3.select("#input");
+//   var cityValue = cityInput.property("value");
 
-  // Filter tableData based on city element
-  var filteredCity = tableData.filter(tableData => tableData.city === cityValue);
+//   // Filter tableData based on city element
+//   var filteredCity = tableData.filter(tableData => tableData.city === cityValue);
   
-  // Loop through filteredCity
-  filteredCity.forEach((cityData) => {
-    var row = tbody.append("tr");
-    Object.entries(cityData).forEach(([key, value]) => {
-      var cell = tbody.append("td");
-      cell.text(value);
-    });
-  });
-});
+//   // Loop through filteredCity
+//   filteredCity.forEach((cityData) => {
+//     var row = tbody.append("tr");
+//     Object.entries(cityData).forEach(([key, value]) => {
+//       var cell = tbody.append("td");
+//       cell.text(value);
+//     });
+//   });
+// });
 
-// CREATE FILTER BY STATE//
-// Function to take input and recreate table
-submit.on("click", function() {
+// // CREATE FILTER BY STATE//
+// // Function to take input and recreate table
+// submit.on("click", function() {
 
-  // Stops page from refreshing
-  d3.event.preventDefault();
+//   // Stops page from refreshing
+//   d3.event.preventDefault();
 
-  // Selects table in tbody tag
-  d3.select(".summary").html("");
+//   // Selects table in tbody tag
+//   d3.select(".summary").html("");
 
-  // Create variables for input element and value
-  var stateInput = d3.select("#state");
-  var stateValue = stateInput.property("value");
+//   // Create variables for input element and value
+//   var stateInput = d3.select("#input");
+//   var stateValue = stateInput.property("value");
   
-  // Filter tableData based on state element
-  var filteredState = tableData.filter(tableData => tableData.state === stateValue);
+//   // Filter tableData based on state element
+//   var filteredState = tableData.filter(tableData => tableData.state === stateValue);
   
-  // Loop through filteredState
-  filteredState.forEach((stateData) => {
-    var row = tbody.append("tr");
-    Object.entries(stateData).forEach(([key, value]) => {
-      var cell = tbody.append("td");
-      cell.text(value);
-    });
-  });
-});
+//   // Loop through filteredState
+//   filteredState.forEach((stateData) => {
+//     var row = tbody.append("tr");
+//     Object.entries(stateData).forEach(([key, value]) => {
+//       var cell = tbody.append("td");
+//       cell.text(value);
+//     });
+//   });
+// });
 
-// CREATE FILTER BY COUNTRY //
-// Function to take input and recreate table
-submit.on("click", function() {
+// // CREATE FILTER BY COUNTRY //
+// // Function to take input and recreate table
+// submit.on("click", function() {
 
-  // Stops page from refreshing
-  d3.event.preventDefault();
+//   // Stops page from refreshing
+//   d3.event.preventDefault();
 
-  // Selects table in tbody tag
-  d3.select(".summary").html("");
+//   // Selects table in tbody tag
+//   d3.select(".summary").html("");
 
-  // Create variables for input element and value
-  var countryInput = d3.select("#country");
-  var countryValue = countryInput.property("value");
+//   // Create variables for input element and value
+//   var countryInput = d3.select("#input");
+//   var countryValue = countryInput.property("value");
 
 
-  // Filter tableData based on country element
-   var filteredCountry = tableData.filter(tableData => tableData.country === countryValue);
+//   // Filter tableData based on country element
+//    var filteredCountry = tableData.filter(tableData => tableData.country === countryValue);
 
-  // Loop through filteredCountry
-  filteredCountry.forEach((countryData) => {
-    var row = tbody.append("tr");
-    Object.entries(countryData).forEach(([key, value]) => {
-      var cell = tbody.append("td");
-      cell.text(value);
-    });
-  });
-});
+//   // Loop through filteredCountry
+//   filteredCountry.forEach((countryData) => {
+//     var row = tbody.append("tr");
+//     Object.entries(countryData).forEach(([key, value]) => {
+//       var cell = tbody.append("td");
+//       cell.text(value);
+//     });
+//   });
+// });
 
-// CREATE FILTER BY SHAPE //
-// Function to take input and recreate table
-submit.on("click", function() {
+// // CREATE FILTER BY SHAPE //
+// // Function to take input and recreate table
+// submit.on("click", function() {
 
-  // Stops page from refreshing
-  d3.event.preventDefault();
+//   // Stops page from refreshing
+//   d3.event.preventDefault();
 
-  // Selects table in tbody tag
-  d3.select(".summary").html("");
+//   // Selects table in tbody tag
+//   d3.select(".summary").html("");
 
-  // Create variables for input element and value
-  var shapeInput = d3.select("#shape");
-  var shapeValue = shapeInput.property("value");
+//   // Create variables for input element and value
+//   var shapeInput = d3.select("#input");
+//   var shapeValue = shapeInput.property("value");
 
-  // Filter tableData based on shape element
-  var filteredShape = tableData.filter(tableData => tableData.shape === shapeValue);
+//   // Filter tableData based on shape element
+//   var filteredShape = tableData.filter(tableData => tableData.shape === shapeValue);
 
-  // Loop through filteredShape
-  filteredShape.forEach((shapeData) => {
-    var row = tbody.append("tr");
-    Object.entries(shapeData).forEach(([key, value]) => {
-      var cell = tbody.append("td");
-      cell.text(value);
-    });
-  });
-});
+//   // Loop through filteredShape
+//   filteredShape.forEach((shapeData) => {
+//     var row = tbody.append("tr");
+//     Object.entries(shapeData).forEach(([key, value]) => {
+//       var cell = tbody.append("td");
+//       cell.text(value);
+//     });
+//   });
+// });

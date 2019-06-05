74450c808090a671e0e8c5245411fa573d5d3dcb
Peter Hardy <peterphardy@gmail.com>
2019-06-04 20:44:24 -0700

    metadata function progress

--- 14 - Plotly Belly Button Biodiversity/static/js/app.js
+++ 14 - Plotly Belly Button Biodiversity/static/js/app.js
@@ -1,23 +1,31 @@
+// Data Route
+var metadata = "/metadata/<sample>";
+
+// Builds the metadata panel
 function buildMetadata(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
-
   // Use `d3.json` to fetch the metadata for a sample
+  d3.json(metadata).then(function(data) {
+
     // Use d3 to select the panel with id of `#sample-metadata`
-
     // Use `.html("") to clear any existing metadata
-
+    d3.select(".panel-body").html("");
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-
+    Object.entries(metadata).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+  })
 }
 
-function buildCharts(sample) {
+function buildCharts() {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  // d3.json()
 
     // @TODO: Build a Bubble Chart using the sample data
 
